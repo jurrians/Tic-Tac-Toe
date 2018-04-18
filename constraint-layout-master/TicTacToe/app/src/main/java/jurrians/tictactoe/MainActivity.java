2555207a436384df521070882f3d5537a7b38dd6
jurrians <jurrianschreuder@gmail.com>
2018-04-18 14:00:14 +0200

    user interface gefixt

---
+++ constraint-layout-master/TicTacToe/app/src/main/java/jurrians/tictactoe/MainActivity.java
@@ -0,0 +1,22 @@
+package jurrians.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void tileClicked(View view) {
+
+    }
+
+    public void resetClicked (View view) {
+
+    }
+}
